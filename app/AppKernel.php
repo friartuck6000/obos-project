bebe17107d5141523d389e6ca4b0c89636133bb2
Kyle Tucker <kyleatucker@gmail.com>
2015-03-28 20:56:23 -0400

    Build timekeeping bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Obos\Bundle\BillingBundle\ObosBillingBundle(),
+            new Obos\Bundle\TimekeepingBundle\ObosTimekeepingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Obos/Bundle/TimekeepingBundle/ObosTimekeepingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Obos\Bundle\TimekeepingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ObosTimekeepingBundle extends Bundle
+{
+}
