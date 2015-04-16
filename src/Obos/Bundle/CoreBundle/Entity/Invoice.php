de6475de8497eda76fce33273896e8b6d008f47d
Kyle Tucker <kyleatucker@gmail.com>
2015-04-16 02:15:28 -0400

    Add stubs for remaining entities

---
+++ src/Obos/Bundle/CoreBundle/Entity/Invoice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Obos\Bundle\CoreBundle\Entity;
+
+
+/**
+ * A bill for work performed on a project.
+ */
+class Invoice
+{}
---
+++ src/Obos/Bundle/CoreBundle/Entity/ProjectAsset.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Obos\Bundle\CoreBundle\Entity;
+
+
+/**
+ * A file asset associated with a project.
+ *
+ */
+class ProjectAsset
+{}
