75b75ff39460169324fef82625f462e2f2db4953
Kyle Tucker <kyleatucker@gmail.com>
2015-04-15 23:21:33 -0400

    Fully define user entities

--- src/Obos/Bundle/CoreBundle/Entity/Consultant.php
+++ src/Obos/Bundle/CoreBundle/Entity/Consultant.php
@@ -11,4 +11,135 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity()
  */
 class Consultant extends User
-{}
+{
+    // Gender choices
+    const GENDER_MALE        = 'M';
+    const GENDER_FEMALE      = 'F';
+    const GENDER_UNDISCLOSED = 'U';
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @var  DateTime  Consultant's date of birth
+     *
+     * @ORM\Column(type="datetime", nullable=TRUE)
+     */
+    protected $dateOfBirth;
+
+    /**
+     * @var  string  Consultant's gender selection; see the GENDER_ constants for a list of possible values.
+     *
+     * @ORM\Column(type="string", length=1, nullable=FALSE)
+     */
+    protected $gender = self::GENDER_UNDISCLOSED;
+
+    /**
+     * @var  string  Consultant's self-proclaimed professional title.
+     *
+     * @ORM\Column(type="string", length=64, nullable=TRUE)
+     */
+    protected $title;
+
+    /**
+     * @var  string  Consultant's specified industry.
+     *
+     * @ORM\Column(type="string", length=32, nullable=TRUE)
+     */
+    protected $industry;
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Get a list of the available options for {@see $gender}.
+     *
+     * @return  string[]
+     */
+    public function getGenderOptions()
+    {
+        return [
+            self::GENDER_MALE,
+            self::GENDER_FEMALE,
+            self::GENDER_UNDISCLOSED
+        ];
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @param   DateTime  $dateOfBirth
+     * @return  $this
+     */
+    public function setDateOfBirth($dateOfBirth)
+    {
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $gender
+     * @return  $this
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $title
+     * @return  $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $industry
+     * @return  $this
+     */
+    public function setIndustry($industry)
+    {
+        $this->industry = $industry;
+
+        return $this;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return  DateTime
+     */
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getIndustry()
+    {
+        return $this->industry;
+    }
+}
--- src/Obos/Bundle/CoreBundle/Entity/User.php
+++ src/Obos/Bundle/CoreBundle/Entity/User.php
@@ -2,7 +2,8 @@
 
 namespace Obos\Bundle\CoreBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping as ORM,
+    DateTime;
 
 
 /**
@@ -23,4 +24,138 @@ class User
     use Template\IdentifierTrait;
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @var  string  User's email address, which doubles as his/her login.
+     *
+     * @ORM\Column(type="string", length=96, nullable=FALSE)
+     */
+    protected $email;
+
+    /**
+     * @var  string  User's (hashed) password.
+     *
+     * @ORM\Column(type="string", length=64, nullable=FALSE)
+     */
+    protected $password;
+
+    /**
+     * @var  string  User's first name.
+     *
+     * @ORM\Column(type="string", length=32, nullable=TRUE)
+     */
+    protected $firstName;
+
+    /**
+     * @var  string  User's last name.
+     *
+     * @ORM\Column(type="string", length=32, nullable=TRUE)
+     */
+    protected $lastName;
+
+    /**
+     * @var  DateTime  The timestamp of the User's last login.
+     *
+     * @ORM\Column(type="datetime", nullable=TRUE)
+     */
+    protected $lastLogin;
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @param   string  $email
+     * @return  $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $password
+     * @return  $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $firstName
+     * @return  $this
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @param   string  $lastName
+     * @return  $this
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @param   DateTime  $lastLogin
+     * @return  $this
+     */
+    public function setLastLogin(DateTime $lastLogin = NULL)
+    {
+        $this->lastLogin = ($lastLogin instanceof DateTime) ? $lastLogin : new DateTime();
+
+        return $this;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * @return  string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return  string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return  DateTime
+     */
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
 }
