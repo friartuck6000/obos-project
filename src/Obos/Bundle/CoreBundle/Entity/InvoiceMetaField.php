c9ed0c970e94ea5e0b435d06550dccdb0b8c683a
Kyle Tucker <kyleatucker@gmail.com>
2015-04-16 02:16:08 -0400

    Add metadata-management stubs

---
+++ src/Obos/Bundle/CoreBundle/Entity/InvoiceMetaField.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Obos\Bundle\CoreBundle\Entity;
+
+
+/**
+ * A field of metadata about an invoice.
+ *
+ */
+class InvoiceMetaField extends Template\MetaField
+{}
---
+++ src/Obos/Bundle/CoreBundle/Entity/PaymentMetaField.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Obos\Bundle\CoreBundle\Entity;
+
+
+/**
+ * A field of metadata about a payment.
+ *
+ */
+class PaymentMetaField extends Template\MetaField
+{}
