8655643b3eda34363fd4f2de08018e05f883b576
Kyle Tucker <kyleatucker@gmail.com>
2015-04-17 02:07:23 -0400

    Build root (login) and registration views

--- src/Obos/Bundle/CoreBundle/Controller/CoreController.php
+++ src/Obos/Bundle/CoreBundle/Controller/CoreController.php
@@ -6,13 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+
+/**
+ * Root app controller
+ *
+ * @Route("/")
+ */
 class CoreController extends Controller
 {
     /**
-     * @Route("/_uitest")
+     * App root. If the user is authorized, she is redirected to the project listing view; otherwise,
+     * the default view prompts her to log in or register.
+     *
+     * @Route("/", name="core_root")
      * @Template()
      */
-    public function uiTestAction()
+    public function rootAction()
+    {
+        return [];
+    }
+
+    /**
+     * Registration page.
+     *
+     * @Route("/register", name="core_register")
+     * @Template()
+     */
+    public function registerAction()
     {
         return [];
     }
