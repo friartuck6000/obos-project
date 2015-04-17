1563ee65e85feac9f2ea4b4e8aad9145f6fb0083
Kyle Tucker <kyleatucker@gmail.com>
2015-04-16 23:19:52 -0400

    Define metadata entities

--- src/Obos/Bundle/CoreBundle/Entity/InvoiceMetaField.php
+++ src/Obos/Bundle/CoreBundle/Entity/InvoiceMetaField.php
@@ -2,10 +2,41 @@
 
 namespace Obos\Bundle\CoreBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * A field of metadata about an invoice.
  *
+ * @ORM\Entity()
+ * @ORM\Table(name="invoice_meta")
  */
 class InvoiceMetaField extends Template\MetaField
-{}
+{
+    /**
+     * @var  Invoice  The invoice this field is attached to.
+     *
+     * @ORM\ManyToOne(targetEntity="Invoice")
+     * @ORM\JoinColumn(name="invoiceID", referencedColumnName="ID", onDelete="CASCADE")
+     */
+    protected $invoice;
+
+    /**
+     * @param   Invoice  $invoice
+     * @return  $this
+     */
+    public function setInvoice($invoice)
+    {
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+
+    /**
+     * @return  Invoice
+     */
+    public function getInvoice()
+    {
+        return $this->invoice;
+    }
+}
--- src/Obos/Bundle/CoreBundle/Entity/PaymentMetaField.php
+++ src/Obos/Bundle/CoreBundle/Entity/PaymentMetaField.php
@@ -2,10 +2,42 @@
 
 namespace Obos\Bundle\CoreBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * A field of metadata about a payment.
  *
+ * @ORM\Entity()
+ * @ORM\Table(name="payment_meta")
  */
 class PaymentMetaField extends Template\MetaField
-{}
+{
+    /**
+     * @var  Payment  The payment this field is attached to.
+     *
+     * @ORM\ManyToOne(targetEntity="Payment")
+     * @ORM\JoinColumn(name="paymentID", referencedColumnName="ID", onDelete="CASCADE")
+     */
+    protected $payment;
+
+    /**
+     * @param   Payment  $payment
+     * @return  $this
+     */
+    public function setPayment($payment)
+    {
+        $this->payment = $payment;
+
+        return $this;
+    }
+
+    /**
+     * @return  Payment
+     */
+    public function getPayment()
+    {
+        return $this->payment;
+    }
+
+}
